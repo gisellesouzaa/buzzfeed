eaa43a890e0a5bbce7f70b93c06ce040faa43186
Giselle Souza <giselle_souza97@yahoo.com.br>
2023-12-20 18:49:46 -0300

    Novo componente: mídias sociais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizzComponent } from './components/quizz/quizz.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuizzComponent,
-    HomeComponent
+    HomeComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
